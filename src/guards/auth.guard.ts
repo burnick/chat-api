b90a7755f62210947d12d1f95269bf912b7d6a95
burnick <arnelceledonio@gmail.com>
2023-04-05 11:55:53 +0800

    feat: added comments for guard code

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,15 +1,17 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { getSession } from 'next-auth/react';
-// import { Observable } from 'rxjs';
 // import Session from '../common/middleware/session.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    //TODO: fix get session does not work
     const request = context.switchToHttp().getRequest();
     const session = await getSession({ req: request });
+
     const myArray = context.getArgs();
     if (!session) {
+      // TODO: fix this hack
       for (let i = 0; i < myArray.length; i++) {
         const myObject = await myArray[i];
         if (myObject && 'sessionUser' in myObject) {
